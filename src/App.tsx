cab5e7722c31f3e80260b5c6a75a6d49a658bca3
Consint-Vishnu <Erukulla.Vishnu@Consint.AI>
2025-03-20 22:27:05 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  const pptViewUrl = "https://consintsolutionspvtltd.sharepoint.com/sites/Consint-Projects/_layouts/15/Doc.aspx?sourcedoc={f7455533-2244-4e03-b559-b872de3d4387}&action=embedview&wdAr=1.33333333333";
-  const pptEditUrl = "https://consintsolutionspvtltd.sharepoint.com/sites/Consint-Projects/_layouts/15/WopiFrame.aspx?sourcedoc={f7455533-2244-4e03-b559-b872de3d4387}&action=edit";
+  const pptViewUrl = "https://consintsolutionspvtltd.sharepoint.com/sites/Consint-Projects/_layouts/15/Doc.aspx?sourcedoc={77f9060a-8428-4bfc-81ce-d64f73691d75}&action=embedview&wdAr=1.3333333333333333";
+  const pptEditUrl = "https://consintsolutionspvtltd.sharepoint.com/sites/Consint-Projects/_layouts/15/WopiFrame.aspx?sourcedoc={77f9060a-8428-4bfc-81ce-d64f73691d75}&action=edit";
 
   return (
     <div className="ppt-container">
